245a5188f7bddf6820b7179c0d679bcae8980829
Your Name <cdaniel@u90z03s07.42kl.edu.my>
2021-06-15 19:53:10 +0800

    rush00

---
+++ rush00/rush01.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int t_x, int t_y)
+{
+	int	c_y;
+	int	c_x;
+
+	c_y = 1;
+	c_x = 1;
+	while (c_y <= t_y)
+	{
+		while (c_x <= t_x)
+		{
+			if ((c_y == 1 && c_x == 1) || (c_y == t_y && c_x == t_x))
+				ft_putchar('/');
+			else if ((c_y == 1 && c_x == t_x) || (c_y == t_y && c_x == 1))
+				ft_putchar('\\');
+			else if ((c_y != 1 && c_y != t_y) && (c_x != 1 && c_x != t_x))
+				ft_putchar(' ');
+			else
+				ft_putchar('*');
+			c_x++;
+		}
+		ft_putchar('\n');
+		c_y++;
+		c_x = 1;
+	}
+}
